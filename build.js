0a4a26ab964b54a5c5bb07d822fec70624310f16
Captain Fantasy <captain@fantasyui.com>
2017-07-23 10:47:11 -0400

    Add postcss-color-function

--- build.js
+++ build.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const postcss = require('postcss');
 const precss = require('precss');
 const autoprefixer = require('autoprefixer');
+const colorFunction = require("postcss-color-function")
 
 fs.readFile('src/app.css', (err, css) => {
-    postcss([precss, autoprefixer])
-        .process(css, { from: 'src/app.css', to: 'dest/app.css' })
+
+    postcss([precss, colorFunction, autoprefixer])
+        .process(css, { from: 'src/app.css', to: 'dist/app.css' })
         .then(result => {
             fs.writeFile('dist/app.css', result.css);
             if ( result.map ) fs.writeFile('dist/app.css.map', result.map);
         });
+
 });
