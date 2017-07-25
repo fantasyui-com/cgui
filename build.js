3aac8e08e922a72db6b51726703422a9ba43d997
Captain Fantasy <captain@fantasyui.com>
2017-07-25 14:01:24 -0400

    Add cssnano and font-logic (npm/web-fonts)

--- build.js
+++ build.js
@@ -3,16 +3,24 @@ const postcss = require('postcss');
 const precss = require('precss');
 const autoprefixer = require('autoprefixer');
 const colorFunction = require("postcss-color-function")
+
+const mergeRules = require('postcss-merge-rules');
 const revolutionize = require('revolutionize');
+const webfonts = require('web-fonts');
+const cssnano = require('cssnano');
+const specification = {
+  import: "url('https://fonts.googleapis.com/css?family=Roboto:100')",
+  family: "'Roboto', sans-serif",
+  selector: 'html, input, button',
+};
 
-
-const revolution = {
+const declaration = {
   theme: 'tint',
   selectors: {
          page: ['.html'],
     container: ['.cgui-container'],
     component: ['.cgui-left', '.cgui-right'],
-        label: ['.cgui-text', '.cgui-muted'],
+        label: ['.cgui-text', '.cgui-muted', 'html, input, button'],
         input: ['.cgui-command'],
        button: ['.cgui-execute'],
   }
@@ -20,10 +28,15 @@ const revolution = {
 
 fs.readFile('src/app.css', (err, css) => {
 postcss([
+
   precss,
   colorFunction,
-  revolutionize(revolution),
-  autoprefixer
+  revolutionize(declaration),
+  webfonts(specification),
+  autoprefixer,
+  mergeRules,
+  cssnano({ preset: 'default' }),
+
 ]).process(css, { from: 'src/app.css', to: 'dist/app.css' })
   .then(result => {
     fs.writeFile('dist/app.css', result.css);
